4e75444878d17ba321cb002bef962f87f9091eb1
sam <sampurna.chapagainnn@gmail.com>
2023-05-19 17:47:20 +0100

    display data from db using query

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Sam',
-        'title': 'Blog Post 1',
-        'content': 'First Post',
-        'date_posted': 'August 27, 2020'
-    }, 
-    {
-        'author': 'Santosh',
-        'title': 'Blog Post 2',
-        'content': 'Second Post',
-        'date_posted': 'August 27, 2022'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Sam',
+#         'title': 'Blog Post 1',
+#         'content': 'First Post',
+#         'date_posted': 'August 27, 2020'
+#     }, 
+#     {
+#         'author': 'Santosh',
+#         'title': 'Blog Post 2',
+#         'content': 'Second Post',
+#         'date_posted': 'August 27, 2022'
+#     }
+# ]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
